9fd6654eaacb8587bcf0d4bb0391138aa29fdbd6
Nanami <royalpankaj63@gmail.com>
2024-11-02 17:26:34 +0530

    catching all segment in the Docs folder

---
+++ app/blog/page.js
@@ -0,0 +1,3 @@
+export default function Blog(){
+    return <h1>Welcome My First Blog</h1>
+}
---
+++ app/product/[productId]/reviews/[reviewId]/page.js
@@ -0,0 +1,5 @@
+export default function ReviewDetails({params}){
+    return <h1>
+        Review {params.reviewId} for product {params.productId} 
+    </h1>
+}
