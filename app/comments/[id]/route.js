781b6b06857486629286126d244338162825e954
Nanami <royalpankaj63@gmail.com>
2024-11-03 15:37:50 +0530

    route handlers for get, post, put, delete

---
+++ app/comments/[id]/route.js
@@ -0,0 +1,23 @@
+import { comments } from "../data";
+
+export async function GET(request,{params}) {
+    const comment=comments.find((comment)=>comment.id===parseInt(params.id))
+    
+    return Response.json(comment);
+
+}
+
+export async function PATCH(request,{params}) {
+    const body=await request.json()
+    const {text}=body
+    const index=comments.findIndex((comment)=>comment.id===parseInt(params.id));
+    comments[index].text=text;
+    return  Response.json(comments[index]);
+}
+
+export async function DELETE(request,{params}) {
+    const index=comments.findIndex((comment)=>comment.id===parseInt(params.id));
+    const deletedComment=comments[index];
+    comments.splice(index,1);
+    return Response.json(deletedComment);
+}
